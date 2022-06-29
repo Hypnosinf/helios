0b24ab4c5eacfe6ac66e8aa1b76910ccbffbebf3
Jair Rojas Ruiz <v-jrojasruiz@microsoft.com>
2022-06-29 16:58:28 -0500

    add git ignore

--- HeliosFunction/Function1.cs
+++ HeliosFunction/Function1.cs
@@ -40,6 +40,8 @@ namespace HeliosFunction
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
+            _logger.LogError("test my error");
+
             return new OkObjectResult(responseMessage);
         }
     }
