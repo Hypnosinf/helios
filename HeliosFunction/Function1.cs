a5365156f72d32e52ec5d47407373c2a859d7409
Jair Rojas <jaiborojas@live.com.mx>
2022-06-30 18:34:32 +0000

    Updated Function1.cs

--- HeliosFunction/Function1.cs
+++ HeliosFunction/Function1.cs
@@ -40,7 +40,7 @@ namespace HeliosFunction
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
-            _logger.LogError("test my error");
+            _logger.LogError("test my error, in another commit");
 
             return new OkObjectResult(responseMessage);
         }
